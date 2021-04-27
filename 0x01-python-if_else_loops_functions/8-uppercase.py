3013666520fd3e8d742d2aeac09b1278cec21533
JakeFC <jacobchavera@yahoo.com>
2021-04-27 20:18:52 +0000

    more using ord and if logic to analyze and change ascii values

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            c = chr(ord(str[i]) - 32)
+        else:
+            c = str[i]
+        print("{}".format(c), end = "")
+    print("")
