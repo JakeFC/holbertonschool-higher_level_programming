a4ab2888f92fcc70d46a327a90bf2017c850874a
JakeFC <jacobchavera@yahoo.com>
2021-04-27 21:16:20 +0000

    added possibility to add a node to an empty list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,12 +10,18 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *trav, *new, *prev;
 
-	if (!*head)
+	if (!head)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	new->n = number;
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+		return (new);
+	}
 	prev = *head;
 	if (prev->n >= number)
 	{
