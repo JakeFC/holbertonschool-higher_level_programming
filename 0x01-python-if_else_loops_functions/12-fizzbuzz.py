42b7d685ab1bbc34c17930a3a04cbbe5f432d29e
JakeFC <jacobchavera@yahoo.com>
2021-04-27 20:35:02 +0000

    added definition to the top

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0:
-        if i % 5 == 0:
-            print("FizzBuzz", end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("FizzBuzz", end=" ")
+            else:
+                print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
+            print(i, end=" ")
