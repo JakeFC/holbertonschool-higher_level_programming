553bfbf6466f09bdadc29e7ec4fe469fc774668e
JakeFC <jacobchavera@yahoo.com>
2021-04-28 23:35:46 +0000

    making a copy of a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    new = my_list.copy()
+    new[idx] = element
+    return new
