9cdf911e08db31165a368714516c08bd691a6206
JakeFC <jacobchavera@yahoo.com>
2021-04-29 00:24:33 +0000

    solved edge case of element length 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
-        print("")
         return
     for i in range(len(matrix)):
         for n in range(len(matrix[i]) - 1):
             print("{:d} ".format(matrix[i][n]), end="")
         if len(matrix[i]):
+            if len(matrix[i]) == 1:
+                n = -1
             print("{:d}".format(matrix[i][n + 1]), end="")
         print("")
