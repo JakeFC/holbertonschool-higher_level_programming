5913b6c36cbf926a0883419da3d1747e17042f57
JakeFC <jacobchavera@yahoo.com>
2021-04-29 01:14:08 +0000

    added check for negative index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is None or len(my_list) is 0 or idx > len(my_list) - 1:
+    if my_list is None or len(my_list) is 0:
+        return
+    idx > len(my_list) - 1 or idx < 0:
         return
     del my_list[idx]
     return my_list
