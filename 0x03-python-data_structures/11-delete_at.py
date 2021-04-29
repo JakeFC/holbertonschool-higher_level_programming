7152b402627bb52072a134cd8ab6118de4bfb720
JakeFC <jacobchavera@yahoo.com>
2021-04-29 01:14:48 +0000

    added 'if' to second if statement

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if my_list is None or len(my_list) is 0:
         return
-    idx > len(my_list) - 1 or idx < 0:
+    if idx > len(my_list) - 1 or idx < 0:
         return
     del my_list[idx]
     return my_list
