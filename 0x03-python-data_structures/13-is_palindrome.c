58e5a4c4a0ac8f376eff073dae0d0bfb8b0ea087
JakeFC <jacobchavera@yahoo.com>
2021-04-29 02:01:28 +0000

    reverted changes

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,15 +8,15 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int i, ii, arr[300];
+	int i, ii, *arr;
 	listint_t *trav = *head;
 
 	if (!head || !*head)
 		return (1);
-/*	for (i = 0; trav; i++)*/
-/*		trav = trav->next;*/
-/*	arr = malloc(sizeof(int) * i);*/
-/*	trav = *head;*/
+	for (i = 0; trav; i++)
+		trav = trav->next;
+	arr = malloc(sizeof(int) * i);
+	trav = *head;
 	for (i = 0; trav; i++)
 	{
 		arr[i] = trav->n;
@@ -24,6 +24,10 @@ int is_palindrome(listint_t **head)
 	}
 	for (--i, ii = 0; ii <= i; ii++, i--)
 		if (arr[ii] != arr[i])
+		{
+			free(arr);
 			return (0);
+		}
+	free(arr);
 	return (1);
 }
