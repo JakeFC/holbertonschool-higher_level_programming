eb1b02e640405241c439721e59d9e21f2ae6268a
JakeFC <jacobchavera@yahoo.com>
2021-04-30 05:01:58 +0000

    added edge case for empty list and divide by 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,4 +5,6 @@ def weight_average(my_list=[]):
     count = 0
     for i in my_list:
         count += i[1]
+    if count is 0:
+        return 0
     return (sum(map(lambda x: x[0] * x[1], my_list)) / count)
