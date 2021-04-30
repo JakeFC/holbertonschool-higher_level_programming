6149d5ecfd865bf01d486043fa94c91ac12e3e4f
JakeFC <jacobchavera@yahoo.com>
2021-04-30 15:48:31 +0000

    optimized using len()

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,4 @@
 def uniq_add(my_list=[]):
     if my_list is None:
         return
-    x = 0
-    for i in set(my_list):
-        x += i
-    return x
+    return len(my_list)
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,7 +2,4 @@
 def number_keys(a_dictionary):
     if a_dictionary is None:
         return
-    count = 0
-    for x in a_dictionary:
-        count += 1
-    return count
+    return len(a_dictionary)
