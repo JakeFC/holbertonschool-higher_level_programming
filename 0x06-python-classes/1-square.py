cd1815c8d185b468cbebc9e266c989ef1cf764d7
JakeFC <jacobchavera@yahoo.com>
2021-05-04 23:39:04 +0000

    creating a private instance attribute on creation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with Square class definition"""
+
+
+class Square:
+    """A class with instance attribute size
+    """
+    def __init__(self, size=0):
+        self.__size = size
