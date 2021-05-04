97eaa9fca6b4c0ab43dc672335d713a596b97478
JakeFC <jacobchavera@yahoo.com>
2021-05-04 23:57:53 +0000

    added method area() to class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module with Square class definition"""
+
+
+class Square:
+    """A class with instance attribute size
+    defined only when valid int is passed as arg
+    and method area
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise TypeError('size must be >= 0')
+        else:
+            raise ValueError('size must be an integer')
+
+    def area(self):
+        return self.__size ** 2
