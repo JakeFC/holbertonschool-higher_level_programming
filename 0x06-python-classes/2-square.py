aa9772623e07b2dfff7f1c3aa537c5f0adff77a5
JakeFC <jacobchavera@yahoo.com>
2021-05-04 23:50:44 +0000

    added back the '__' to make size private

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     def __init__(self, size=0):
         if isinstance(size, int):
             if size >= 0:
-                self.size = size
+                self.__size = size
             else:
                 raise TypeError('size must be >= 0')
         else:
