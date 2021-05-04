4764561aef205033e4d8dafda84772e9f193abd7
JakeFC <jacobchavera@yahoo.com>
2021-05-04 23:49:34 +0000

    setting conditional attribute or raising error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module with Square class definition"""
+
+
+class Square:
+    """A class with instance attribute size
+    defined only when valid int is passed as arg
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.size = size
+            else:
+                raise TypeError('size must be >= 0')
+        else:
+            raise ValueError('size must be an integer')
