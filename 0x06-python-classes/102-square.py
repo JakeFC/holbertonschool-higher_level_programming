7aa623de6c0446c5b7fb02c31ecdc9fdcacfbcdc
JakeFC <jacobchavera@yahoo.com>
2021-05-05 03:33:41 +0000

    defining comparisons

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Module with Square class definition"""
+
+
+class Square:
+    """A class with:
+    __size: optional private instance attribute
+    area(self): method to perform function
+    size(self): property used to return __size
+    size(self, value): method to set a new __size
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise TypeError('size must be >= 0')
+        else:
+            raise ValueError('size must be an integer')
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__init__(value)
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
