160778dd6a5aa1c98ed9065186b776274ce7fa24
JakeFC <jacobchavera@yahoo.com>
2021-05-06 05:15:19 +0000

    fixed method names

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,11 +9,11 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """A test case for max_integer function tests"""
-    def normal_test(self):
+    def test_normal(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def zero_test(self):
-        self.assertEqual(max_integer([]), none)
+    def test_zero(self):
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == "__main__":
     unittest.main()
