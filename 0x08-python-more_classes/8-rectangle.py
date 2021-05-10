68f78f339194ba500ca13f2a71921c3b14071ed2
JakeFC <jacobchavera@yahoo.com>
2021-05-10 02:27:30 +0000

    using static method

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Module with Rectangle class definition"""
+
+
+class Rectangle:
+    """A class with private instance attributes:
+    width: int width of the object
+    height: int height of the object
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        print('Bye rectangle...')
+        Rectangle.number_of_instances -= 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width is 0 or self.height is 0:
+            return 0
+        return self.width * 2 + self.height * 2
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    def __str__(self):
+        if self.width is 0 or self.height is 0:
+            return ""
+        return ("\n".join([str(self.print_symbol) * self.width for i in
+                           range(self.height)]))
+
+    def __repr__(self):
+        return "Rectangle(%d, %d)" % (self.width, self.height)
