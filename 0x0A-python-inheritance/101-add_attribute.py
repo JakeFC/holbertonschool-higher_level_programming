6a97fb7adb27deb6f7e14b29f3d941f6c59d12ee
JakeFC <jacobchavera@yahoo.com>
2021-05-11 14:06:30 +0000

    added function documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,9 @@
 
 
 def add_attribute(obj, attribute, value):
+    """Checks whether object is of builtin class and whether its class
+    defines attribute name under __slots__ before setting new attribute
+    """
     if obj.__class__.__module__ == 'builtins':
         raise TypeError('can\'t add new attribute')
     if hasattr(obj, '__slots__'):
