902d3d5130bac3eed1e5f085d3a28b8c10d009ae
JakeFC <jacobchavera@yahoo.com>
2021-05-11 12:48:32 +0000

    overwriting a magic method and referencing a slice of it in the metaclass

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Subclass of Rectangle with private instance attribute size"""
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    def __str__(self):
+        return '[Square]' + super().__str__()[11:]
