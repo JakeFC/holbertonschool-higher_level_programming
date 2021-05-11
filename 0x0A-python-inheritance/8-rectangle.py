7011f44ea83df33337101f209e307012221c4ef0
JakeFC <jacobchavera@yahoo.com>
2021-05-11 12:17:23 +0000

    fixed pep8 spacing

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Module for BaseGeometry and Rectangle classes"""
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Subclass of BaseGeometry with private instance attributes width and
     height
