a53fd4634369cd17fc06f3132aff80dbd880fd28
JakeFC <jacobchavera@yahoo.com>
2021-05-12 06:37:27 +0000

    removed a couple redundant lines

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,9 +9,6 @@ def add_attribute(obj, attribute, value):
     if obj.__class__.__module__ == 'builtins':
         raise TypeError('can\'t add new attribute')
     if hasattr(obj, '__slots__'):
-        if attribute in obj.__slots__:
-            setattr(obj, attribute, value)
-            return
-        else:
+        if attribute not in obj.__slots__:
             raise TypeError('can\'t add new attribute')
     setattr(obj, attribute, value)
