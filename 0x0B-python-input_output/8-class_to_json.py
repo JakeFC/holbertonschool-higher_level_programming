670cab6e365ad7459caf7e73e1e58a854d92bdea
JakeFC <jacobchavera@yahoo.com>
2021-05-13 01:31:24 +0000

    using the __dict__ attribute of a class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pythyon3
+"""Module for function class_to_json"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure for
+    JSON serialization of object [obj]"""
+    return (obj.__dict__)
