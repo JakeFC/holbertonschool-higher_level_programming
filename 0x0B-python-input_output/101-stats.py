f13fd246c0363d665282ea045d8bda0a683c8dde
JakeFC <jacobchavera@yahoo.com>
2021-05-13 16:04:37 +0000

    removed json import

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Script which reads stdin line by line and computes metrics"""
 import sys
-import json
 
 size = 0
 codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0,
