ae36ec3014347ef0b93cb24b3027c713b09b9420
JakeFC <jacobchavera@yahoo.com>
2021-05-13 00:06:36 +0000

    reading from a file

---
+++ 0x0B-python-input-output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints to stdout"""
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
