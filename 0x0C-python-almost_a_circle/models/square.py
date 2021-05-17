b8f4ac8a5b96953eda410bc4b76d29be6bc7db6b
JakeFC <jacobchavera@yahoo.com>
2021-05-17 06:23:20 +0000

    added public method update()

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,20 @@ class Square(Rectangle):
         return ('[Square] (' + str(self.id) + ') ' + str(self.x) + '/' +
                 str(self.y) + ' - ' + str(self.size))
 
+    def update(self, *args, **kwargs):
+        """assigns attributes from values in args or (if none found) kwargs"""
+        if args and args[0]:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except:
+                pass
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
     @property
     def size(self):
         return self.width
