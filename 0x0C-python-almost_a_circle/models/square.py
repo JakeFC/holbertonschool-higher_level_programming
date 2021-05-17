226440d74fc26cca7c014e163a4e4840765fbb05
JakeFC <jacobchavera@yahoo.com>
2021-05-17 06:18:18 +0000

    added public getter and setter for size attribute and specified __str__ output more to avoid errors

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,4 +9,18 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return '[Square]' + super().__str__()[11:-2]
+        return ('[Square] (' + str(self.id) + ') ' + str(self.x) + '/' +
+                str(self.y) + ' - ' + str(self.size))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            Rectangle.t_error('width')
+        if value <= 0:
+            Rectangle.v_error1('width')
+        self.width = value
+        self.height = value
