f146c0e2cc4b5c4a1a22bb50465c197a53ce1f7d
JakeFC <jacobchavera@yahoo.com>
2021-05-27 22:05:06 +0000

    changed NOT NULL to DEFAULT 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- creates a table id_not_null with id that can't be null and name
+-- creates a table id_not_null with id default value 1 and name
 -- DROP TABLE IF EXISTS <table>;
 DROP TABLE IF EXISTS id_not_null;
 -- CREATE TABLE <table> (VALUES);
-CREATE TABLE id_not_null (id INT NOT NULL, name VARCHAR(256));
+CREATE TABLE id_not_null (id INT DEFAULT 1, name VARCHAR(256));
