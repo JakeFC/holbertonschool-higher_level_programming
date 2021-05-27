2c7e95a5fec664f57353ed82369975abf22bdee2
JakeFC <jacobchavera@yahoo.com>
2021-05-27 21:27:11 +0000

    added second comment line

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- lists all priveleges of user_0d_1 and user_0d_2 on my server in locallhost
+-- SHOW GRANTS FOR <'username'@'hostname'>
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
