42e1ca732346734e15005fedc32449d072cb1a52
JakeFC <jacobchavera@yahoo.com>
2021-05-28 00:37:34 +0000

    joining 3 tables the same way with different SELECT & WHERE statements

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,15 @@
+-- lists all Comedy shows
+-- SELECT <value>
+SELECT title
+-- FROM <table> INNER JOIN <table>
+FROM tv_shows s INNER JOIN tv_show_genres gs
+-- ON <condition>
+ON s.id = gs.show_id
+-- INNER JOIN <table>
+INNER JOIN tv_genres g
+-- ON <condition>
+ON gs.genre_id = g.id
+-- WHERE <condition>
+WHERE g.name = 'Comedy'
+-- ORDER BY <value>
+ORDER BY title;
