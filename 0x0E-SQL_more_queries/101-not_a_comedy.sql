2774f0970a3b1f7949efcc34b12c0901ab750c95
JakeFC <jacobchavera@yahoo.com>
2021-05-28 02:19:14 +0000

    added indents to subqueries

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,17 +5,17 @@ SELECT title
 FROM tv_shows
 -- WHERE <conditions>
 WHERE title NOT IN
--- SELECT <value>
-(SELECT title
--- FROM <table> INNER JOIN <table>
-FROM tv_shows s INNER JOIN tv_show_genres gs
--- ON <condition>
-ON s.id = gs.show_id
--- INNER JOIN <table>
-INNER JOIN tv_genres g
--- ON <condition>
-ON gs.genre_id = g.id
--- WHERE <condition>
-WHERE g.name = 'Comedy')
+      -- (SELECT <value>
+      (SELECT title
+      -- FROM <table> INNER JOIN <table>
+      FROM tv_shows s INNER JOIN tv_show_genres g
+      -- ON <condition>
+      ON s.id = gs.show_id
+      -- INNER JOIN <table>
+      INNER JOIN tv_genres g
+      -- ON <condition>
+      ON gs.genre_id = g.id
+      -- WHERE <condition>)
+      WHERE g.name = 'Comedy')
 -- ORDER BY <value>
 ORDER BY title;
