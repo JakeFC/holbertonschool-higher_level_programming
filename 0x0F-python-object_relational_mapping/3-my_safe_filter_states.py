d5aea906857bf9a54d4d673d9c100f88559d907f
JakeFC <jacobchavera@yahoo.com>
2021-05-31 06:06:39 +0000

    swapped string substitution for comma-escaped for SQL injection security

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script which prints each state whose name matches argv[4] with its id from
+the given db in argv[3], using argv[1] and [2] for MySQL user and passwd,
+respectively. Execute parameters are escaped to avoid SQL injection.
+"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states WHERE BINARY name = %s",
+                   (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    db.close
