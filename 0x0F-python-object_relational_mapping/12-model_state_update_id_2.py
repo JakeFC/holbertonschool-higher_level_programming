1dfd1c889a86a695894c9a45523a9ba4ff3fdd47
JakeFC <jacobchavera@yahoo.com>
2021-05-31 19:26:46 +0000

    querying to get row before changing attributes and commiting

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script which takes MySQL login and database info from argv to change
+a State's name in the states table
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new = State(name='Louisiana')
+    instance = session.query(State).filter_by(id=2).first()
+    instance.name = 'New Mexico'
+    session.commit()
