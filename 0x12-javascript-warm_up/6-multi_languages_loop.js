65fbf1ae48d7ac81fc3c316381eb4940cf4c827d
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:29:56 +0000

    looping through a list with for/in

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+prints 3 lines using an array of a string and a loop
+*/
+const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (idx in array) {
+    console.log(array[idx]);
+}
