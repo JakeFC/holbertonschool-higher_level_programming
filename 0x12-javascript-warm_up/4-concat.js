3af71e810eedb98dc9cbe1cb7f286082231da698
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:07:25 +0000

    switched to actualy string addition ('is' was quoting)

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // prints 2 arguments passed to it, separated by 'is'
-console.log(process.argv[2], 'is', process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
