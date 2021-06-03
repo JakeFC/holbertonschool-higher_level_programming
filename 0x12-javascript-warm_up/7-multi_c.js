858431fadb00c2964bb8cbeb71e30a171e0e2f1e
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:54:29 +0000

    updated if to catch non-numbers

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ prints "C is fun" x times, where x is the first argument, if it converts to
 int, else prints "missing number of occurences"
 */
 const num = parseInt(process.argv[2]);
-if (num !== null) {
+if (num || num === 0) {
   for (let x = 0; x < num; x++) {
     console.log('C is fun');
   }
