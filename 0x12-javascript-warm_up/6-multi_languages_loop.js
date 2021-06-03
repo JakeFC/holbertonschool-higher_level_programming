8456e0e0ea5abf55e0bbb729873805161ff84d95
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:30:30 +0000

    fixed semistandard format

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 /*
 prints 3 lines using an array of a string and a loop
 */
-const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (idx in array) {
-    console.log(array[idx]);
+  console.log(array[idx]);
 }
