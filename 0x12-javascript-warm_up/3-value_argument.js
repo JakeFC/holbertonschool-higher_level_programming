48aa8b8ba920432fc6dfe3e8dbaf8e42da7ba39b
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:06:07 +0000

    updated comment

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// prints first argument passed
+// prints first argument passed, or 'No argument' if none
 const msg = process.argv[2];
 msg ? console.log(msg) : console.log('No argument');
