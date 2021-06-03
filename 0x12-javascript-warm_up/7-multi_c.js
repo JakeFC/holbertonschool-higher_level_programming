5356e6a0400d59a04aaa3a8cbf9eb92951cb2fa5
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:40:21 +0000

    checking for null and running a command in a variable list

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+prints "C is fun" x times, where x is the first argument, if it converts to
+int, else prints "missing number of occurences"
+*/
+const num = parseInt(process.argv[2]);
+if (num !== null) {
+  for (let x = 0; x < num; x++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
+}
