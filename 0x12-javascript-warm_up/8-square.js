6b7f6c059c92c3e7ffadbd59efc8ad11c06ea7e4
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:53:19 +0000

    changes if logic to catch non-numbers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints a square of X's the size of first argument, else "Missing size"
 const num = parseInt(process.argv[2]);
-if (num !== null) {
+if (num || num === 0) {
   for (let y = 0; y < num; y++) {
     for (let x = 0; x < num; x++) {
       process.stdout.write('X');
