8640854d509f3f5cb03f7ef70f43bf28e0d67d12
JakeFC <jacobchavera@yahoo.com>
2021-06-03 01:31:50 +0000

    defined idx before using

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,7 @@
 prints 3 lines using an array of a string and a loop
 */
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let idx = 0;
 for (idx in array) {
   console.log(array[idx]);
 }
