f010b8fc455509ab8c0746be24afe4166f42cf22
JakeFC <jacobchavera@yahoo.com>
2021-06-03 02:17:59 +0000

    fixed indents

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // prints the factorial of an argument
-function factorial(n) {
-    if (!n || n <= 0) {
-        return 1;
-    }
-    return n * factorial (n - 1);
+function factorial (n) {
+  if (!n || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
