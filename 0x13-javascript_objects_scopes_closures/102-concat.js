83a29b8076118349e3b6c2dd8ad2674c0551b546
JakeFC <jacobchavera@yahoo.com>
2021-06-04 01:37:45 +0000

    removed newlines from text that made output match example

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // concats 2 files, passed as 1st and 2nd arg, into file at 3rd arg
 const fs = require('fs');
-let data = fs.readFileSync(process.argv[2]).toString() + '\n';
-data += (fs.readFileSync(process.argv[3]).toString() + '\n');
+let data = fs.readFileSync(process.argv[2]).toString();
+data += fs.readFileSync(process.argv[3]).toString();
 fs.writeFile(process.argv[4], data, function (err) {
   if (err) {
     return console.error(err);
