e59f3f0a5f5e6e9d2962c4304a6a47218f32ba24
JakeFC <jacobchavera@yahoo.com>
2021-06-04 01:35:20 +0000

    reading and writing from files in js, using fs module

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// concats 2 files, passed as 1st and 2nd arg, into file at 3rd arg
+const fs = require('fs');
+let data = fs.readFileSync(process.argv[2]).toString() + '\n';
+data += (fs.readFileSync(process.argv[3]).toString() + '\n');
+fs.writeFile(process.argv[4], data, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+});
