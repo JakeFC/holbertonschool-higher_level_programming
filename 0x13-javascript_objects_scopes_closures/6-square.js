d747d9ce3a91e03b996d6d90ec84bf1afe50f660
JakeFC <jacobchavera@yahoo.com>
2021-06-04 00:08:24 +0000

    changed undefined check on c to pass format check

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,9 @@
 const oldSquare = require('./5-square');
 module.exports = class Square extends oldSquare {
   charPrint (c) {
-    c ? c = c : c = 'X';
+    if (!c) {
+      c = 'X';
+    }
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
         process.stdout.write(c);
